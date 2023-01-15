cd657a2d552052fe6e4b3751095f7a192147ab13
Raphael Rossato Caetano <rossato.caetano@gmail.com>
2023-01-15 11:43:28 -0300

    add more items on products page and change meta data

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import { ProductsList } from "./components/ProductsList";
 import { FooterComponent } from "./components/Footer";
 
 /* IMAGES IMPORTS */
-import img_1 from "./img/img_1.jpg"
-import img_2 from "./img/img_2.jpg"
-import img_3 from "./img/img_3.jpg"
-
+import img_1 from "./img/img_1.jpg";
+import img_2 from "./img/img_2.jpg";
+import img_3 from "./img/img_3.jpg";
+import img_4 from "./img/img_4.jpg";
+import img_5 from "./img/img_5.jpg";
+import img_6 from "./img/img_6.jpg";
 
 const logo = 'MUSIC SHOP';
 const links = ['#sobre', '#produtos', '#contatos'];
 const items = ['Sobre', 'Produtos', 'Contatos']
 
-const fakeData = ['Violão Rossato', 'Madeira especial', 'Corda Nobre', 'Braço dez', 'Tarracha zica'];
+const fakeData2 = ['Violão Rossato', 'Madeira especial', 'Corda Nobre', 'Braço dez', 'Tarracha zica'];
+const fakeData = ['Lorem ipsum', 'Dolor sit', 'Nunc molestie', 'Quisque sed', 'Sed sed efficitur']
+
 
 function App() {
   return (
@@ -27,6 +31,9 @@ function App() {
         <Products image={img_1} items={fakeData}></Products>
         <Products image={img_2} items={fakeData}></Products>
         <Products image={img_3} items={fakeData}></Products>
+        <Products image={img_4} items={fakeData}></Products>
+        <Products image={img_5} items={fakeData}></Products>
+        <Products image={img_6} items={fakeData}></Products>
       </ProductsList>
       <FooterComponent id="contatos" ></FooterComponent>
     </>    
